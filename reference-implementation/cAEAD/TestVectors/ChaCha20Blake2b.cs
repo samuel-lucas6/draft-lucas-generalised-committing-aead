4a6ee6d3264113ddbeaa1fa841689876bc6142f1
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2022-08-13 10:11:04 +0100

    reference-implementation: Use Geralt.
    Also, use spans, switch to BinaryPrimitives instead of BitConverter, and improve max input validation.

--- reference-implementation/cAEAD/TestVectors/ChaCha20Blake2b.cs
+++ reference-implementation/cAEAD/TestVectors/ChaCha20Blake2b.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using cAEAD;
+using Geralt;
 
 namespace TestVectors;
 
@@ -15,10 +16,12 @@ public class ChaCha20Blake2b
         byte[] key = Convert.FromHexString("1001000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
         byte[] ciphertext = Convert.FromHexString("18337327ef02753bf8d996db218a3697c18943ea6efc86a7e449cb67a7592b9e1715a07771797c93789350528e2e7a8d25b4ca7a7d2968776d50577946cb5da693f1e09309236b7bdb04001a8feeab7de48946f08df1cfd0ce03a719232ea7106efb8706e40d7cb6");
-        byte[] computedCiphertext = ChaCha20BLAKE2b.Encrypt(plaintext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(ciphertext, computedCiphertext));
-        byte[] computedPlaintext = ChaCha20BLAKE2b.Decrypt(computedCiphertext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(plaintext, computedPlaintext));
+        var computedCiphertext = new byte[plaintext.Length + BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Encrypt(computedCiphertext, plaintext, nonce, key, associatedData);
+        Assert.IsTrue(ciphertext.SequenceEqual(computedCiphertext));
+        var computedPlaintext = new byte[computedCiphertext.Length - BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Decrypt(computedPlaintext, computedCiphertext, nonce, key, associatedData);
+        Assert.IsTrue(plaintext.SequenceEqual(computedPlaintext));
     }
     
     [TestMethod]
@@ -29,10 +32,12 @@ public class ChaCha20Blake2b
         byte[] key = Convert.FromHexString("1001000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
         byte[] ciphertext = Convert.FromHexString("a1ad6c7c4a9bb8201cf72904ebea1fed709c75ded85adaea7034bdbba1b5ec4f");
-        byte[] computedCiphertext = ChaCha20BLAKE2b.Encrypt(plaintext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(ciphertext, computedCiphertext));
-        byte[] computedPlaintext = ChaCha20BLAKE2b.Decrypt(computedCiphertext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(plaintext, computedPlaintext));
+        var computedCiphertext = new byte[plaintext.Length + BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Encrypt(computedCiphertext, plaintext, nonce, key, associatedData);
+        Assert.IsTrue(ciphertext.SequenceEqual(computedCiphertext));
+        var computedPlaintext = new byte[computedCiphertext.Length - BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Decrypt(computedPlaintext, computedCiphertext, nonce, key, associatedData);
+        Assert.IsTrue(plaintext.SequenceEqual(computedPlaintext));
     }
     
     [TestMethod]
@@ -43,10 +48,12 @@ public class ChaCha20Blake2b
         byte[] key = Convert.FromHexString("1001000000000000000000000000000000000000000000000000000000000000");
         byte[] associatedData = Convert.FromHexString("76312e302e30");
         byte[] ciphertext = Convert.FromHexString("c0deb4501fe4cc651687cff8c9f5377072d4788cfe2d0f51dd97fab7b16fab84");
-        byte[] computedCiphertext = ChaCha20BLAKE2b.Encrypt(plaintext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(ciphertext, computedCiphertext));
-        byte[] computedPlaintext = ChaCha20BLAKE2b.Decrypt(computedCiphertext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(plaintext, computedPlaintext));
+        var computedCiphertext = new byte[plaintext.Length + BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Encrypt(computedCiphertext, plaintext, nonce, key, associatedData);
+        Assert.IsTrue(ciphertext.SequenceEqual(computedCiphertext));
+        var computedPlaintext = new byte[computedCiphertext.Length - BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Decrypt(computedPlaintext, computedCiphertext, nonce, key, associatedData);
+        Assert.IsTrue(plaintext.SequenceEqual(computedPlaintext));
     }
     
     [TestMethod]
@@ -57,10 +64,12 @@ public class ChaCha20Blake2b
         byte[] key = Convert.FromHexString("1001000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
         byte[] ciphertext = Convert.FromHexString("db685e0ff12fafd611a832c90e6c7905598ed65babdf6d8cf7057d07b5168673727dda3ef3d6ed2520332c8036e2ce0f72c413290bc4ae41d2d398e4cb2d1f6e906e232ae471ca0ea4ade513d685a4fab9a886fa885b6f6b54ff04d66612cfdde669bd0dbda23f54");
-        byte[] computedCiphertext = ChaCha20BLAKE2b.Encrypt(plaintext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(ciphertext, computedCiphertext));
-        byte[] computedPlaintext = ChaCha20BLAKE2b.Decrypt(computedCiphertext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(plaintext, computedPlaintext));
+        var computedCiphertext = new byte[plaintext.Length + BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Encrypt(computedCiphertext, plaintext, nonce, key, associatedData);
+        Assert.IsTrue(ciphertext.SequenceEqual(computedCiphertext));
+        var computedPlaintext = new byte[computedCiphertext.Length - BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Decrypt(computedPlaintext, computedCiphertext, nonce, key, associatedData);
+        Assert.IsTrue(plaintext.SequenceEqual(computedPlaintext));
     }
     
     [TestMethod]
@@ -71,59 +80,66 @@ public class ChaCha20Blake2b
         byte[] key = Convert.FromHexString("1002000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
         byte[] ciphertext = Convert.FromHexString("308319762a72faf302e6d34c2f882c27addc1b2130549e55a084bcdc189c2da0497fdbab20989f24a25f2d3934ac825caaf46ec61a853a06eb97b14c2ced147b94c2223506862d32af0be2b1f658597412e65d77560844eee38a190063300c8e8a8c62ea25b943b3");
-        byte[] computedCiphertext = ChaCha20BLAKE2b.Encrypt(plaintext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(ciphertext, computedCiphertext));
-        byte[] computedPlaintext = ChaCha20BLAKE2b.Decrypt(computedCiphertext, nonce, key, associatedData);
-        Assert.IsTrue(CryptographicOperations.FixedTimeEquals(plaintext, computedPlaintext));
+        var computedCiphertext = new byte[plaintext.Length + BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Encrypt(computedCiphertext, plaintext, nonce, key, associatedData);
+        Assert.IsTrue(ciphertext.SequenceEqual(computedCiphertext));
+        var computedPlaintext = new byte[computedCiphertext.Length - BLAKE2b.TagSize];
+        ChaCha20BLAKE2b.Decrypt(computedPlaintext, computedCiphertext, nonce, key, associatedData);
+        Assert.IsTrue(plaintext.SequenceEqual(computedPlaintext));
     }
 
     [TestMethod]
     public void TestVector6()
     {
         byte[] ciphertext = Convert.FromHexString("408319762a72faf302e6d34c2f882c27addc1b2130549e55a084bcdc189c2da0497fdbab20989f24a25f2d3934ac825caaf46ec61a853a06eb97b14c2ced147b94c2223506862d32af0be2b1f658597412e65d77560844eee38a190063300c8e8a8c62ea25b943b3");
+        var plaintext = new byte[ciphertext.Length - BLAKE2b.TagSize];
         byte[] nonce = Convert.FromHexString("000000000000000000000000");
         byte[] key = Convert.FromHexString("1002000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(ciphertext, nonce, key, associatedData));
+        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(plaintext, ciphertext, nonce, key, associatedData));
     }
     
     [TestMethod]
     public void TestVector7()
     {
         byte[] ciphertext = Convert.FromHexString("308319762a72faf302e6d34c2f882c27addc1b2130549e55a084bcdc189c2da0497fdbab20989f24a25f2d3934ac825caaf46ec61a853a06eb97b14c2ced147b94c2223506862d32af0be2b1f658597412e65d77560844eee38a190063300c8e8a8c62ea25b943b4");
+        var plaintext = new byte[ciphertext.Length - BLAKE2b.TagSize];
         byte[] nonce = Convert.FromHexString("000000000000000000000000");
         byte[] key = Convert.FromHexString("1002000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(ciphertext, nonce, key, associatedData));
+        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(plaintext, ciphertext, nonce, key, associatedData));
     }
     
     [TestMethod]
     public void TestVector8()
     {
         byte[] ciphertext = Convert.FromHexString("308319762a72faf302e6d34c2f882c27addc1b2130549e55a084bcdc189c2da0497fdbab20989f24a25f2d3934ac825caaf46ec61a853a06eb97b14c2ced147b94c2223506862d32af0be2b1f658597412e65d77560844eee38a190063300c8e8a8c62ea25b943b3");
+        var plaintext = new byte[ciphertext.Length - BLAKE2b.TagSize];
         byte[] nonce = Convert.FromHexString("000000000000000000000001");
         byte[] key = Convert.FromHexString("1002000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(ciphertext, nonce, key, associatedData));
+        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(plaintext, ciphertext, nonce, key, associatedData));
     }
     
     [TestMethod]
     public void TestVector9()
     {
         byte[] ciphertext = Convert.FromHexString("308319762a72faf302e6d34c2f882c27addc1b2130549e55a084bcdc189c2da0497fdbab20989f24a25f2d3934ac825caaf46ec61a853a06eb97b14c2ced147b94c2223506862d32af0be2b1f658597412e65d77560844eee38a190063300c8e8a8c62ea25b943b3");
+        var plaintext = new byte[ciphertext.Length - BLAKE2b.TagSize];
         byte[] nonce = Convert.FromHexString("000000000000000000000000");
         byte[] key = Convert.FromHexString("1003000000000000000000000000000000000000000000000000000000000000");
         byte[]? associatedData = null;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(ciphertext, nonce, key, associatedData));
+        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(plaintext, ciphertext, nonce, key, associatedData));
     }
     
     [TestMethod]
     public void TestVector10()
     {
         byte[] ciphertext = Convert.FromHexString("308319762a72faf302e6d34c2f882c27addc1b2130549e55a084bcdc189c2da0497fdbab20989f24a25f2d3934ac825caaf46ec61a853a06eb97b14c2ced147b94c2223506862d32af0be2b1f658597412e65d77560844eee38a190063300c8e8a8c62ea25b943b3");
+        var plaintext = new byte[ciphertext.Length - BLAKE2b.TagSize];
         byte[] nonce = Convert.FromHexString("000000000000000000000000");
         byte[] key = Convert.FromHexString("1002000000000000000000000000000000000000000000000000000000000000");
         byte[] associatedData = Convert.FromHexString("76312e302e30");
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(ciphertext, nonce, key, associatedData));
+        Assert.ThrowsException<CryptographicException>(() => ChaCha20BLAKE2b.Decrypt(plaintext, ciphertext, nonce, key, associatedData));
     }
 }
--- reference-implementation/cAEAD/cAEAD/ChaCha20BLAKE2b.cs
+++ reference-implementation/cAEAD/cAEAD/ChaCha20BLAKE2b.cs
@@ -1,107 +1,96 @@
 ﻿using System.Text;
+using System.Buffers.Binary;
 using System.Security.Cryptography;
-using Sodium;
+using Geralt;
 
 namespace cAEAD;
 
-// This reference implementation is using libsodium as the cryptographic library for ChaCha20 and BLAKE2b
-// ChaCha20Ietf refers to the unauthenticated ChaCha20 from RFC 8439
-// GenericHash refers to keyed BLAKE2b-256 from RFC 7693
+// This reference implementation uses libsodium as the cryptographic library for ChaCha20 and BLAKE2b
 public static class ChaCha20BLAKE2b
 {
-    // Constants no matter the cipher and collision resistant, hash-based MAC
-    // C# arrays cannot be greater than int.MaxValue by default
+    // Constants no matter the cipher and collision-resistant, hash-based MAC
     private const int K_LEN = 32;
-    private const int A_MAX = int.MaxValue;
     private const int T_LEN = 32;
+    private const int UInt64BytesLength = 8;
+    private const int BothUInt64BytesLengths = UInt64BytesLength * 2;
     
     // Constants specific to ChaCha20-BLAKE2b
+    // C# arrays cannot be greater than int.MaxValue
     private const int N_MIN = 12;
-    private const int P_MAX = int.MaxValue - T_LEN;
+    private const int P_MAX = int.MaxValue - BothUInt64BytesLengths - T_LEN;
     private const int C_MAX = P_MAX + T_LEN;
     private const string ENCRYPTION_CONTEXT = "ChaCha20.Encrypt()";
     private const string MAC_CONTEXT = "BLAKE2b-256.KeyedHash()";
-
-    public static byte[] Encrypt(byte[] plaintext, byte[] nonce, byte[] key, byte[]? associatedData = null)
+    
+    public static void Encrypt(Span<byte> ciphertext, ReadOnlySpan<byte> plaintext, ReadOnlySpan<byte> nonce, ReadOnlySpan<byte> key, ReadOnlySpan<byte> associatedData = default)
     {
-        if (plaintext == null || plaintext.Length >= P_MAX) { throw new ArgumentOutOfRangeException(nameof(plaintext), $"The {nameof(plaintext)} length must be less than {P_MAX}."); }
-        if (nonce == null || nonce.Length != N_MIN) { throw new ArgumentOutOfRangeException(nameof(nonce), $"The {nameof(nonce)} length must be equal to {N_MIN}."); }
-        if (key == null || key.Length != K_LEN) { throw new ArgumentOutOfRangeException(nameof(key), $"The {nameof(key)} length must be equal to {K_LEN}."); }
-        if (associatedData != null && associatedData.Length >= A_MAX) { throw new ArgumentOutOfRangeException(nameof(associatedData), $"The {nameof(associatedData)} length must be less than {A_MAX}."); }
-        associatedData ??= Array.Empty<byte>();
+        if (plaintext.Length >= P_MAX) { throw new ArgumentOutOfRangeException(nameof(plaintext), $"The {nameof(plaintext)} length must be less than {P_MAX}."); }
+        if (ciphertext.Length != plaintext.Length + T_LEN) { throw new ArgumentOutOfRangeException(nameof(ciphertext), $"The {nameof(ciphertext)} length must be equal to {plaintext.Length + T_LEN}."); }
+        if (nonce.Length != N_MIN) { throw new ArgumentOutOfRangeException(nameof(nonce), $"The {nameof(nonce)} length must be equal to {N_MIN}."); }
+        if (key.Length != K_LEN) { throw new ArgumentOutOfRangeException(nameof(key), $"The {nameof(key)} length must be equal to {K_LEN}."); }
+        if (associatedData != default) { int overflowCheck = checked(plaintext.Length + associatedData.Length + BothUInt64BytesLengths); }
         
-        (byte[] encryptionKey, byte[] macKey) = DeriveKeys(key, nonce);
+        Span<byte> encryptionKey = stackalloc byte[K_LEN], macKey = stackalloc byte[K_LEN];
+        DeriveKeys(encryptionKey, macKey, nonce, key);
         
-        byte[] ciphertext = StreamEncryption.EncryptChaCha20Ietf(plaintext, nonce, encryptionKey);
+        Span<byte> ciphertextNoTag = new byte[plaintext.Length];
+        ChaCha20.Encrypt(ciphertextNoTag, plaintext, nonce, encryptionKey);
         
-        byte[] tag = ComputeTag(associatedData, ciphertext, macKey);
+        Span<byte> tag = stackalloc byte[T_LEN];
+        ComputeTag(tag, associatedData, ciphertextNoTag, macKey);
         
         CryptographicOperations.ZeroMemory(encryptionKey);
         CryptographicOperations.ZeroMemory(macKey);
         
-        return Concat(ciphertext, tag);
+        if (ciphertextNoTag.Length == 0) {
+            tag.CopyTo(ciphertext);
+        }
+        else {
+            Spans.Concat(ciphertext, ciphertextNoTag, tag);
+        }
     }
     
-    public static byte[] Decrypt(byte[] ciphertext, byte[] nonce, byte[] key, byte[]? associatedData = null)
+    public static void Decrypt(Span<byte> plaintext, ReadOnlySpan<byte> ciphertext, ReadOnlySpan<byte> nonce, ReadOnlySpan<byte> key, ReadOnlySpan<byte> associatedData = default)
     {
-        if (ciphertext == null || ciphertext.Length >= C_MAX) { throw new ArgumentOutOfRangeException(nameof(ciphertext), $"The {nameof(ciphertext)} length must be less than {C_MAX}."); }
-        if (nonce == null || nonce.Length != N_MIN) { throw new ArgumentOutOfRangeException(nameof(nonce), $"The {nameof(nonce)} length must be equal to {N_MIN}."); }
-        if (key == null || key.Length != K_LEN) { throw new ArgumentOutOfRangeException(nameof(key), $"The {nameof(key)} length must be equal to {K_LEN}."); }
-        if (associatedData != null && associatedData.Length >= A_MAX) { throw new ArgumentOutOfRangeException(nameof(associatedData), $"The {nameof(associatedData)} length must be less than {A_MAX}."); }
-        associatedData ??= Array.Empty<byte>();
+        if (ciphertext.Length >= C_MAX) { throw new ArgumentOutOfRangeException(nameof(ciphertext), $"The {nameof(ciphertext)} length must be less than {C_MAX}."); }
+        if (plaintext.Length != ciphertext.Length - T_LEN) { throw new ArgumentOutOfRangeException(nameof(plaintext), $"The {nameof(plaintext)} length must be equal to {ciphertext.Length - T_LEN}."); }
+        if (nonce.Length != N_MIN) { throw new ArgumentOutOfRangeException(nameof(nonce), $"The {nameof(nonce)} length must be equal to {N_MIN}."); }
+        if (key.Length != K_LEN) { throw new ArgumentOutOfRangeException(nameof(key), $"The {nameof(key)} length must be equal to {K_LEN}."); }
+        if (associatedData != default) { int overflowCheck = checked(plaintext.Length + associatedData.Length + BothUInt64BytesLengths); }
         
-        var tag = new byte[T_LEN];
-        Array.Copy(ciphertext, sourceIndex: ciphertext.Length - tag.Length, tag, destinationIndex: 0, tag.Length);
+        ReadOnlySpan<byte> tag = ciphertext.Slice(start: ciphertext.Length - T_LEN);
         
-        var ciphertextNoTag = new byte[ciphertext.Length - tag.Length];
-        Array.Copy(ciphertext, ciphertextNoTag, ciphertextNoTag.Length);
+        ReadOnlySpan<byte> ciphertextNoTag = ciphertext.Slice(start: 0, ciphertext.Length - tag.Length);
         
-        (byte[] encryptionKey, byte[] macKey) = DeriveKeys(key, nonce);
-
-        byte[] computedTag = ComputeTag(associatedData, ciphertextNoTag, macKey);
+        Span<byte> encryptionKey = stackalloc byte[K_LEN], macKey = stackalloc byte[K_LEN];
+        DeriveKeys(encryptionKey, macKey, nonce, key);
+        
+        Span<byte> computedTag = stackalloc byte[T_LEN];
+        ComputeTag(computedTag, associatedData, ciphertextNoTag, macKey);
         
         CryptographicOperations.ZeroMemory(macKey);
         
-        if (CryptographicOperations.FixedTimeEquals(tag, computedTag) == false)
-        {
+        if (ConstantTime.Equals(tag, computedTag) == false) {
             CryptographicOperations.ZeroMemory(encryptionKey);
             throw new CryptographicException("Authentication failed.");
         }
-        // Overwriting the ciphertext with the plaintext to avoid creating another array
-        ciphertextNoTag = StreamEncryption.DecryptChaCha20Ietf(ciphertextNoTag, nonce, encryptionKey);
+        
+        ChaCha20.Decrypt(plaintext, ciphertextNoTag, nonce, encryptionKey);
         CryptographicOperations.ZeroMemory(encryptionKey);
-        return ciphertextNoTag;
     }
 
-    private static (byte[] encryptionKey, byte[] macKey) DeriveKeys(byte[] key, byte[] nonce)
+    private static void DeriveKeys(Span<byte> encryptionKey, Span<byte> macKey, ReadOnlySpan<byte> nonce, ReadOnlySpan<byte> key)
     {
-        byte[] encryptionKey = GenericHash.Hash(Encoding.UTF8.GetBytes(ENCRYPTION_CONTEXT), key, K_LEN);
-        byte[] macKey = GenericHash.Hash(Concat(Encoding.UTF8.GetBytes(MAC_CONTEXT), nonce), key, K_LEN);
-        return (encryptionKey, macKey);
+        BLAKE2b.ComputeTag(encryptionKey, Encoding.UTF8.GetBytes(ENCRYPTION_CONTEXT), key);
+        BLAKE2b.ComputeTag(macKey, Arrays.Concat(Encoding.UTF8.GetBytes(MAC_CONTEXT), nonce.ToArray()), key);
     }
     
-    private static byte[] ComputeTag(byte[] associatedData, byte[] ciphertext, byte[] macKey)
+    private static void ComputeTag(Span<byte> tag, ReadOnlySpan<byte> associatedData, ReadOnlySpan<byte> ciphertext, ReadOnlySpan<byte> macKey)
     {
-        return GenericHash.Hash(Concat(associatedData, ciphertext, LE64(associatedData.Length), LE64(ciphertext.Length)), macKey, T_LEN);
-    }
-    
-    private static byte[] LE64(int x)
-    {
-        var xBytes = BitConverter.GetBytes((ulong) x);
-        // BitConverter automatically detects the computer's architecture
-        if (!BitConverter.IsLittleEndian) { Array.Reverse(xBytes); }
-        return xBytes;
-    }
-    
-    private static T[] Concat<T>(params T[][] arrays)
-    {
-        int offset = 0;
-        var result = new T[arrays.Sum(array => array.Length)];
-        foreach (var array in arrays)
-        {
-            Array.Copy(array, sourceIndex: 0, result, destinationIndex: offset, array.Length);
-            offset += array.Length;
-        }
-        return result;
+        byte[] associatedDataLength = new byte[UInt64BytesLength], ciphertextLength = new byte[UInt64BytesLength];
+        BinaryPrimitives.WriteUInt64LittleEndian(associatedDataLength, (ulong)associatedData.Length);
+        BinaryPrimitives.WriteUInt64LittleEndian(ciphertextLength, (ulong)ciphertext.Length);
+        Span<byte> message = Arrays.Concat(associatedData.ToArray(), ciphertext.ToArray(), associatedDataLength, ciphertextLength);
+        BLAKE2b.ComputeTag(tag, message, macKey);
     }
 }
